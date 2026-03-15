81532738c971727868465ec45319ba037a211d45
Noah Burrell <noah@burrell.tech>
2026-03-15 15:26:10 -0500

    Change checkmarks for arrows
    Signed-off-by: Noah Burrell <noah@burrell.tech>

--- js/components.js
+++ js/components.js
@@ -19,7 +19,7 @@
   document.body.insertAdjacentHTML('afterbegin',
     '<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" style="display:none">' +
       '<symbol id="icon-check" viewBox="0 0 20 20">' +
-        '<path fill="currentColor" fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"/>' +
+        '<path fill="currentColor" fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"/>' +
       '</symbol>' +
     '</svg>'
   );
