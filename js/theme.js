6a8674c46a4914962c94a6b81cf7bd3235f8061e
Noah Burrell <noah@burrell.tech>
2026-03-16 15:32:17 -0500

    Fix some style and loading bugs
    Signed-off-by: Noah Burrell <noah@burrell.tech>

--- js/theme.js
+++ js/theme.js
@@ -2,5 +2,7 @@
   var s = localStorage.getItem('theme');
   if (s === 'dark' || (s === null && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     document.documentElement.classList.add('dark');
+    document.documentElement.style.colorScheme = 'dark';
+    document.documentElement.style.backgroundColor = '#030712';
   }
 }());
